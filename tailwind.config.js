8e705b18dd09e4d39b265bf1c2fc0e1869cf546b
RitobrotoMukherjee <ritobrotomukherjee1991@gmail.com>
2022-07-25 20:48:45 +0530

    Setup tailwind css and logo

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+/* eslint-disable */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#4d6da7",
+        fontColor: "#FFFFFF"
+      }
+    },
+  },
+  plugins: [],
+}
