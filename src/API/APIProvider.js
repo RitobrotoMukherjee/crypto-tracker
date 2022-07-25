d642af7a70b9a141af4a94549ed0851327596efb
RitobrotoMukherjee <ritobrotomukherjee1991@gmail.com>
2022-07-25 21:33:18 +0530

    Add API logics to fetch data for both pages

---
+++ src/API/APIProvider.js
@@ -0,0 +1,16 @@
+const LIST_API = "https://api.coinstats.app/public/v1/coins";
+const DETAILS_API = "https://api.coinstats.app/public/v1/markets";
+
+const getList = async () => {
+    const response = await fetch(LIST_API);
+    return response.json();
+}
+
+const getDetails = async (coinId) => {
+    const response = await fetch(`${DETAILS_API}?coinId=${coinId}`);
+    return response.json();
+}
+
+export { getList, getDetails };
+
+export default { 'Content-Type': 'application/json' };
---
+++ src/components/Details.js
@@ -0,0 +1,1 @@
+console.log('Details js');
