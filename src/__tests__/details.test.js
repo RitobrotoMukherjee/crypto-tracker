6a2a6d60d7a7566cadf18fa63cb9cd79090e73a7
RitobrotoMukherjee <ritobrotomukherjee1991@gmail.com>
2022-07-27 19:19:37 +0530

    Add tests for reducers and snapshot test for components

---
+++ src/__tests__/details.test.js
@@ -0,0 +1,33 @@
+import { DETAILS_DATA } from "../API/INITIAL_DATA";
+import details from "../redux/details/details";
+
+describe("Starting Details Page test suit", () => {
+    test('Testing initial data to be empty list and loading true', () => {
+        // Arrange
+        const expectedData = {
+            loading: true,
+            coinDetails: [],
+        };
+
+        // ACT
+        const reducedData = details(undefined, { type: undefined, coinDetails: undefined });
+
+        // Assert
+        expect(reducedData).toEqual(expectedData);
+        expect(reducedData.loading).toBeTruthy();
+    });
+
+    test('Mock API data should be returned', () => {
+        // Arrange
+        const expectedData = DETAILS_DATA;
+        const GET_DETAILS_ACTION = 'crypto/details/FETCH_DATA';
+
+
+        // ACT
+        const reducedData = details(undefined, { type: GET_DETAILS_ACTION, loading: false, coinDetails: expectedData });
+
+        // Assert
+        expect(reducedData.coinDetails).toEqual(expectedData);
+        expect(reducedData.loading).toBeFalsy();
+    })
+})
---
+++ src/__tests__/snapshots.test.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import TestRenderer from 'react-test-renderer';
+import { Provider } from 'react-redux';
+import store from '../redux/ConfigureStore';
+import HomePage from '../components/Home';
+import Loading from '../components/Loading';
+import NoDataFound from '../components/NoDataFound';
+
+
+describe("Main components snapshot testing", () => {
+    it('Test if The Home Page renders correctly', () => {
+        const TREE = TestRenderer.create(
+            <Provider store={store}>
+                <BrowserRouter>
+                    <HomePage />
+                </BrowserRouter>
+            </Provider>,
+        );
+        expect(TREE).toMatchSnapshot();
+    });
+
+    it('Test if The Home Page renders correctly', () => {
+        const TREE = TestRenderer.create(
+            <Provider store={store}>
+                <BrowserRouter>
+                    <Loading />
+                </BrowserRouter>
+            </Provider>,
+        );
+        expect(TREE).toMatchSnapshot();
+    });
+
+    it('Test if The Home Page renders correctly', () => {
+        const TREE = TestRenderer.create(
+            <Provider store={store}>
+                <BrowserRouter>
+                    <NoDataFound text="Test Snapshot No Data Found" />
+                </BrowserRouter>
+            </Provider>,
+        );
+        expect(TREE).toMatchSnapshot();
+    });
+});
